d66e2830bc2b56f49d085198e208aa3baea10ec3
toziol <thibaut.oziol@gmail.com>
2021-12-10 11:36:43 +0100

    Update Day5.java

--- src/main/java/fr/toz/adventofcode/Day5.java
+++ src/main/java/fr/toz/adventofcode/Day5.java
@@ -2,6 +2,6 @@ package fr.toz.adventofcode;
 
 public class Day5 {
 	public static void main(String[] args) {
-		
+		System.out.println("Voir le package de test pour la résolution du jour 5 !");
 	}
 }
